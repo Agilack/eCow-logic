3e91143efcd5b72373125e6e0c248b6506b3ba5a
Hooligan-0 <gwen-github@agilack.fr>
2016-01-02 23:07:15 +0100

    = UPD: decode more functions, mainly for memory write

--- pico-ice40-1k/sw-probe-unit_test/sw-ut_iap/iap-rev/iap.s
+++ pico-ice40-1k/sw-probe-unit_test/sw-ut_iap/iap-rev/iap.s
@@ -32,11 +32,11 @@ hnd_cmd3: /* ERAS_SECT ? */
     b.n		hnd_ret
 
 hnd_cmd4:
-    cmp	r0, #19
+    cmp		r0, #19
     bne.n	hnd_cmd5        /* 0x1fff1034 */
     adds	r0, r1, #0
-    bl	fct_ERAS_BLCK
-    b.n	hnd_ret
+    bl		fct_ERAS_BLCK
+    b.n		hnd_ret
 hnd_cmd5:
     adds	r5, r0, #0
     subs	r5, #20
@@ -49,10 +49,10 @@ hnd_cmd_prog:
     movs	r0, #1
     cmp		r4, #34	/* 0x22 */
     bne.n	hnd_ret         /* 0x1fff1054 */
-    adds	r0, r1, #0
-    adds	r1, r2, #0
-    adds	r2, r3, #0
-    bl		0x1fff1164
+    adds	r0, r1, #0 /* Dest addr to R0           */
+    adds	r1, r2, #0 /* Data source address to R1 */
+    adds	r2, r3, #0 /* Length to R2              */
+    bl		fct_PROG
 hnd_ret:
     pop		{r3, r4, r5, pc}
     nop
@@ -202,145 +202,188 @@ fct_PROG: /* @ 0x1fff1164 */
     mov		r6, r9
     mov		r5, r8
     push	{r5, r6, r7}
-    mov		sl, r0
-    mov		r8, r2
+    mov		sl, r0  /* Save target address to sl */
+    mov		r8, r2  /* Save length to r8         */
+
     ldr		r3, [pc, #348]	/* Load Flash controller base address (0x1fff12d0) */
     ldr		r2, [r3, #0]    /* FACCR */
     movs	r0, #192	/* 0xc0 */
     ands	r0, r2
     mov		ip, r0
-    /* Key0 */
-    ldr		r2, [pc, #340]	/* (0x1fff12d4) */
-    str		r2, [r3, #48]	/* 0x30 */
-    /* Key 1 */
-    ldr		r2, [pc, #340]	        /* (0x1fff12d8) */
-    str		r2, [r3, #52]	/* 0x34 */
-
-    ldr		r0, [r3, #0]
+    /* Write Key0  */
+    ldr		r2, [pc, #340]	/* Get key0 (@ 0x1fff12d4) */
+    str		r2, [r3, #48]	/* Write it to FKEYR0 (register offset 0x30) */
+    /* Write Key 1 */
+    ldr		r2, [pc, #340]  /* Get key1 (@ 0x1fff12d8) */
+    str		r2, [r3, #52]	/* Write it to FKEYR1 (register offset 0x34) */
+    /* Configure FACCR */
+    ldr		r0, [r3, #0]    /* FACCR                 */
     ldr		r2, [pc, #340]	/* fffcff03 (0x1fff12dc) */
-    ands	r2, r0
-    ldr		r0, [pc, #340]	/* (0x1fff12e0) */
-    orrs	r2, r0
-    str		r2, [r3, #0]
+    ands	r2, r0          /* clear bits            */
+    ldr		r0, [pc, #340]	/* 00030004 (0x1fff12e0) */
+    orrs	r2, r0          /* set bits              */
+    str		r2, [r3, #0]    /* Write back FACCR      */
+    /* Set FADDR */
     mov		r0, sl
     str		r0, [r3, #4]
-    movs	r3, #3
+
+    /* test destination addresse alignement */
+    movs	r3, #3         /* Is the address aligned to 32bits ?*/
     ands	r3, r0
-    beq.n	0x1fff11d4
-    movs	r2, #4
+    beq.n	fct_PROG_step1 /* branch */
+    movs	r2, #4         /* addr of the previous 32b aligned */
     subs	r3, r2, r3
-    beq.n	0x1fff11d8
+    beq.n	fct_PROG_step2
+    /* Is data length = 0 */
     mov		r2, r8
     cmp		r2, #0
-    beq.n	0x1fff1296
-    mov		r9, r8
+    beq.n	fct_PROG_complete
+
+/* here @ 11a6 */
+    mov		r9, r8          /* Copy data length to r9 */
     adds	r7, r3, #0
     movs	r3, #0
-    movs	r5, #0
-    ldr		r4, [pc, #288]	/* (0x1fff12d0) */
+    movs	r5, #0          /* Reset counter */
+    ldr		r4, [pc, #288]	/* Flash controller base address (0x1fff12d0) */
     movs	r2, #128	/* 0x80         */
     movs	r6, #3
-      	ldrb	r3, [r1, r3]
-      	str	r3, [r4, #8]
-      	str	r2, [r4, #12]
-      	ldr	r3, [r4, #16]
-      	cmp	r3, #0
-      	bge.n	0x1fff11ba
-    ldr		r0, [r4, #16]
-    ands	r0, r6
+fct_PROG_l: /* @ 11b4 */
+    ldrb	r3, [r1, r3]    /* Read from data source (r1) */
+    str		r3, [r4, #8]    /* Write data to DATAR        */
+    str		r2, [r4, #12]   /* write 0x80 to FCTRLR       */
+fct_PROG_wait_rdy: /* @ 11ba */
+    ldr		r3, [r4, #16]   /* Read FSTATR */
+    cmp		r3, #0          /* is 0 ? */
+    bge.n	fct_PROG_wait_rdy  /* yes :( wait more ... */
+
+    ldr		r0, [r4, #16]   /* Read status again          */
+    ands	r0, r6          /* Only bits 0 and 1 are used */
     bne.n	0x1fff127e
-    adds	r5, #1
+
+    adds	r5, #1          /* Increment counter */
     subs	r3, r5, #0
-      	cmp	r3, r7
-      	beq.n	0x1fff11da
-      	cmp	r5, r9
-      	bne.n	0x1fff11b4
-      	b.n	0x1fff11da
-      	movs	r5, #0
-      	b.n	0x1fff11da
-      	movs	r5, #0
-      	mov	r3, r8
-      	subs	r6, r3, r5
-      	ldr	r3, [pc, #240]	/* (0x1fff12d0) */
-      	ldr	r2, [pc, #240]	/* (0x1fff12d4) */
-      	str	r2, [r3, #48]	/* 0x30 */
-      	ldr	r2, [pc, #240]	/* (0x1fff12d8) */
-      	str	r2, [r3, #52]	/* 0x34 */
-      	ldr	r2, [r3, #0]
-      	movs	r0, #252	/* 0xfc */
-      	bics	r2, r0
-      	movs	r0, #12
-      	orrs	r2, r0
-      	str	r2, [r3, #0]
-      	mov	r0, sl
-      	adds	r2, r5, r0
-      	str	r2, [r3, #4]
-      	cmp	r6, #3
-      	ble.n	0x1fff1238
-      	adds	r4, r1, r5
+    cmp		r3, r7
+    beq.n	fct_PROG_write
+
+    cmp		r5, r9
+    bne.n	fct_PROG_l /* go back to fct_PROG_l */
+    b.n		fct_PROG_write
+
+fct_PROG_step1: /* @ 0x1fff11d4 */
+    movs	r5, #0   /* Init counter */
+    b.n		fct_PROG_write
+fct_PROG_step2: /* @ 0x1fff11d8 */
+    movs	r5, #0   /* Init counter */
+fct_PROG_write: /* @ 0x1fff11da */
+    mov		r3, r8
+    subs	r6, r3, r5
+    ldr		r3, [pc, #240]	/* Flash controller base addres (0x1fff12d0) */
+    /* Write key0 */
+    ldr		r2, [pc, #240]	/* key0 (0x1fff12d4) */
+    str		r2, [r3, #48]	/* 0x30 */
+    /* Write key1 */
+    ldr		r2, [pc, #240]	/* (0x1fff12d8) */
+    str		r2, [r3, #52]	/* 0x34 */
+    /* Update FACCR */
+    ldr		r2, [r3, #0]    /* Read FACCR */
+    movs	r0, #252	/* 0xfc */
+    bics	r2, r0
+    movs	r0, #12
+    orrs	r2, r0
+    str		r2, [r3, #0]    /* Write back FACCR */
+
+    mov		r0, sl
+    adds	r2, r5, r0
+    str		r2, [r3, #4]    /* Update FADDR */
+    cmp		r6, #3
+    ble.n	fct_PROG_1238
+
+    /* Programming loop 32bits per transfer */
+
+    adds	r4, r1, r5
+    adds	r2, r3, #0      /* Copy Flash controller base to R2 */
+    movs	r3, #128	/* 0x80 */
+    mov		r9, r3
+    movs	r7, #3
+fct_PROG_write_32: /* @ 1208 */
+    ldrb	r0, [r4, #3]
+    lsls	r0, r0, #24
+    ldrb	r3, [r4, #2]
+    lsls	r3, r3, #16
+    adds	r3, r0, r3
+    ldrb	r0, [r4, #1]
+    lsls	r0, r0, #8
+    adds	r3, r3, r0
+    ldrb	r0, [r4, #0]
+    adds	r3, r3, r0
+    str		r3, [r2, #8]  /* Write 4 last bytes to DATAR */
+
+    mov		r0, r9
+    str		r0, [r2, #12] /* write FCTRLR */
+fct_PROG_wrdy_2:
+    ldr		r3, [r2, #16] /* read  FSTATR */
+    cmp		r3, #0
+    bge.n	fct_PROG_wrdy_2
+    /* Verify  STAT result */
+    ldr		r0, [r2, #16] /* read FSTATR (again) */
+    ands	r0, r7        /* only bits 0 and 1 */
+    bne.n	0x1fff127e
+
+    adds	r5, #4
+    subs	r6, #4
+    adds	r4, #4
+    cmp		r6, #3
+    bgt.n	fct_PROG_write_32
+
+fct_PROG_1238: /* @ 0x1fff1238 */
+    ldr		r3, [pc, #148]	/* Flash controller base address (0x1fff12d0) */
+    /* Write key0 */
+    ldr		r2, [pc, #152]	/* (0x1fff12d4) */
+    str		r2, [r3, #48]	/* 0x30 */
+    /* Write key1 */
+    ldr		r2, [pc, #152]	/* (0x1fff12d8) */
+    str		r2, [r3, #52]	/* 0x34 */
+    /* Update FACCR */
+    ldr		r0, [r3, #0]    /* Read FACCR */
+    ldr		r2, [pc, #148]	/* Mask fffcff03 (0x1fff12dc) */
+    ands	r2, r0
+    ldr		r0, [pc, #148]	/* Value 00030004 (0x1fff12e0) */
+    orrs	r2, r0
+    str		r2, [r3, #0]    /* Write back FACCR */
+    /* */
+    mov		r2, sl
+    adds	r7, r5, r2
+    str		r7, [r3, #4] /* Update FADDR ... ? */
+
+    movs	r0, #0
+    cmp		r6, #0
+    ble.n	fct_PROG_127e
+
+    /* Programming loop 8bits per transfer */
+
+    adds	r4, r1, r5
+    adds	r6, r1, r6
+    adds	r5, r6, r5
     adds	r2, r3, #0
-      	movs	r3, #128	/* 0x80 */
-      	mov	r9, r3
-      	movs	r7, #3
-      	ldrb	r0, [r4, #3]
-      	lsls	r0, r0, #24
-      	ldrb	r3, [r4, #2]
-      	lsls	r3, r3, #16
-      	adds	r3, r0, r3
-      	ldrb	r0, [r4, #1]
-      	lsls	r0, r0, #8
-      	adds	r3, r3, r0
-      	ldrb	r0, [r4, #0]
-      	adds	r3, r3, r0
-      	str	r3, [r2, #8]
-      	mov	r0, r9
-    str	r0, [r2, #12]
-      	ldr	r3, [r2, #16]
-      	cmp	r3, #0
-      	bge.n	0x1fff1222
-      	ldr	r0, [r2, #16]
-      	ands	r0, r7
-      	bne.n	0x1fff127e
-      	adds	r5, #4
-      	subs	r6, #4
-      	adds	r4, #4
-      	cmp	r6, #3
-      	bgt.n	0x1fff1208
-      	ldr	r3, [pc, #148]	/* (0x1fff12d0) */
-      	ldr	r2, [pc, #152]	/* (0x1fff12d4) */
-      	str	r2, [r3, #48]	/* 0x30 */
-      	ldr	r2, [pc, #152]	/* (0x1fff12d8) */
-    str	r2, 	[r3, #52]	/* 0x34 */
-      	ldr	r0, [r3, #0]
-      	ldr	r2, [pc, #148]	/* (0x1fff12dc) */
-      	ands	r2, r0
-      	ldr	r0, [pc, #148]	/* (0x1fff12e0) */
-      	orrs	r2, r0
-      	str	r2, [r3, #0]
-      	mov	r2, sl
-      	adds	r7, r5, r2
-      	str	r7, [r3, #4]
-      	movs	r0, #0
-      	cmp	r6, #0
-      	ble.n	0x1fff127e
-      	adds	r4, r1, r5
-      	adds	r6, r1, r6
-      	adds	r5, r6, r5
-      	adds	r2, r3, #0
-      	movs	r6, #128	/* 0x80 */
-      	movs	r7, #3
-      	ldrb	r3, [r4, #0]
-      	str	r3, [r2, #8]
-      	str	r6, [r2, #12]
-      	ldr	r3, [r2, #16]
-      	cmp	r3, #0
-    bge.n	0x1fff126c
-      	ldr	r0, [r2, #16]
-      	ands	r0, r7
-      	bne.n	0x1fff127e
-      	adds	r4, #1
-      	cmp	r4, r5
-      	bne.n	0x1fff1266
+    movs	r6, #128	/* 0x80 */
+    movs	r7, #3
+fct_PROG_1266:
+    ldrb	r3, [r4, #0]
+    str		r3, [r2, #8]  /* write DATAR  */
+    str		r6, [r2, #12] /* write FCTRLR */
+fct_PROG_wrdy3:
+    ldr		r3, [r2, #16] /* read FSTATR  */
+    cmp		r3, #0
+    bge.n	fct_PROG_wrdy3
+    /* Werify STAT */
+    ldr		r0, [r2, #16]
+    ands	r0, r7
+    bne.n	0x1fff127e
+    adds	r4, #1
+    cmp		r4, r5
+    bne.n	fct_PROG_1266
+
+fct_PROG_127e: /* @ 0x1fff127e */
      	ldr	r3, [pc, #80]	/* (0x1fff12d0) */
       	ldr	r2, [pc, #80]	/* (0x1fff12d4) */
       	str	r2, [r3, #48]	/* 0x30 */
@@ -352,7 +395,8 @@ fct_PROG: /* @ 0x1fff1164 */
       	mov	r1, ip
       	orrs	r1, r2
       	str	r1, [r3, #0]
-      	b.n	0x1fff12c6
+      	b.n	fct_prog_exit
+fct_PROG_complete: /* @ 1296 */
       	ldr	r3, [pc, #56]	/* (0x1fff12d0) */
       	ldr	r0, [pc, #56]	/* (0x1fff12d4) */
       	str	r0, [r3, #48]	/* 0x30 */
@@ -377,19 +421,20 @@ fct_PROG: /* @ 0x1fff1164 */
       	str	r1, [r3, #4]
       	movs	r0, #0
       	b.n	0x1fff127e
+fct_PROG_exit: /* @ 12c6 */
       	pop	{r2, r3, r4}
       	mov	r8, r2
       	mov	r9, r3
       	mov	sl, r4
       	pop	{r4, r5, r6, r7, pc}
 
-fct_PROG_var1: .word 0x50004100 /* 12d0 */
-fct_PROG_var2: .word 0x71755253
-fct_PROG_var3: .word 0x75fca918
-fct_PROG_var4: .word 0xff03fffc
-fct_PROG_var5: .word 0x00040003
+fct_PROG_FACCR: .word 0x41005000 /* @ 12d0 */
+fct_PROG_var2: .word 0x52537175
+fct_PROG_var3: .word 0xa91875fc
+fct_PROG_var4: .word 0xfffcff03
+fct_PROG_var5: .word 0x00030004
 
-some_fct_2: /* @ 0x1fff12e0 */
+some_fct_2: /* @ 0x1fff12e4 */
     push	{r3, r4, r5, lr}
     cmp		r1, #0
     beq.n	0x1fff12fc

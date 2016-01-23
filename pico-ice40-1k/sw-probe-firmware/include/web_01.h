ce099f406a33490630b08149317e86813904d6a9
Hooligan-0 <gwen-github@agilack.fr>
2016-01-23 10:29:14 +0100

    = UPD: Add a small form to main web page that allow sending SPI data more easily

--- pico-ice40-1k/sw-probe-firmware/include/web_01.h
+++ pico-ice40-1k/sw-probe-firmware/include/web_01.h
@@ -2,9 +2,17 @@
 const char web_01[] =
   "<html>"
   "<body>"
+    "<div><h3>Charger PLD</h3>"
     "<form action=\"/pld\" method=\"POST\" enctype=\"multipart/form-data\">"
       "<input type=\"file\" name=\"bit\" accept=\"\">"
       "<input type=\"submit\">"
     "</form>"
+    "</div>"
+    "<div><h3>Commande SPI</h3>"
+    "<form action=\"/spi\" method=\"POST\">"
+      "<input type=\"text\" name=\"cmd\">"
+      "<input type=\"submit\">"
+    "</form>"
+    "</div>"
   "</body>"
   "</html>";

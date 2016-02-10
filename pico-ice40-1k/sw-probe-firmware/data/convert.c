9fee15c245c8f4c9f743e83fb97a7c85c0076903
Hooligan-0 <gwen-github@agilack.fr>
2016-02-10 18:44:07 +0100

    = UPD: Improve web page (add toolbar and split design by function)

--- pico-ice40-1k/sw-probe-firmware/data/convert.c
+++ pico-ice40-1k/sw-probe-firmware/data/convert.c
@@ -46,6 +46,9 @@ int main(int argc, char **argv)
 	insert_file("web/index.html", "home.htm");
 	insert_file("web/styl.css",   "styl.css");
 	insert_file("web/ecow.png",   "ecow.png");
+	insert_file("web/home.png",   "home.png");
+	insert_file("web/rand.png",   "rand.png");
+	insert_file("web/qust.png",   "qust.png");
 	
 	fwrite(image, 1, current_offset, f_out);
 	
